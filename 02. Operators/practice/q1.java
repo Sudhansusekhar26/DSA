7655d457ac77b4a934a15ae616beb16473dbaaec
Sudhansusekhar Jena <sudhansusekharjena411@gmail.com>
2026-06-11 21:31:45 +0530

    Update q1.java

--- 02. Operators/practice/q1.java
+++ 02. Operators/practice/q1.java
@@ -7,7 +7,7 @@ public class q1 {
       int exp1 = (x * y / x); 
       int exp2 = (x * (y / x)); 
  
-System.out.println(exp1); 
+System.out.println(exp1+" "); 
 System.out.println(exp2);
     }
 }
